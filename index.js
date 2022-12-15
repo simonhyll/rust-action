bfea9907ff760aecd9453a36c674a9a34cfa65e7
Simon Hyll <hyllsimon@gmail.com>
2022-12-15 18:49:49 +0100

    Test

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { spawn, exec } = require('node:child_process');
 exec('git lfs pull', (error, stdout, stderr) => {
     const inputs = []
     for (const [key, value] of Object.entries(process.env)) {
-        if (key.startsWith('INPUT_')) {
+        if (key.startsWith('INPUT_') && value !== "") {
             inputs.push(`--${key.slice(6).toLowerCase()}="${value}"`)
         }
     }
@@ -13,5 +13,5 @@ exec('git lfs pull', (error, stdout, stderr) => {
     let platform = "ubuntu-latest"
     if (process.platform === "darwin") platform = "macos-latest"
     if (process.platform === "win32") platform = "windows-latest.exe"
-    spawn(`./bin/rust-action-${platform}`, inputs, { stdio: 'inherit' })
+    spawn(`./bin/rust-action-${platform}`, ["build", ...inputs], { stdio: 'inherit' })
 })
