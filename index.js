06166e7eb92ee73c0e681d7a8535907a6ce812c5
Simon Hyll <hyllsimon@gmail.com>
2022-12-15 17:35:59 +0100

    Initial commit

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const { exec } = require('node:child_process');
 
-exec('bin/rust-action', (error, stdout, stderr) => {
+exec('git submodule init && git submodule update', (error, stdout, stderr) => {
     if (error) {
         console.error(`exec error: ${error}`);
         return;
     }
-    console.log(`${stdout}`);
-});
+    exec('bin/rust-action', (error, stdout, stderr) => {
+        if (error) {
+            console.error(`exec error: ${error}`);
+            return;
+        }
+        console.log(`${stdout}`);
+    })
+})
+
